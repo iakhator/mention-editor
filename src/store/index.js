2dc31746c19e06244a2e410ed05580700236a6e0
iakhator <iakhator@gmail.com>
2024-09-18 22:21:16 +0100

    fix store

--- src/store/index.js
+++ src/store/index.js
@@ -23,71 +23,133 @@ class MentionableStore {
     this.content = value;
   }
 
-  renderHTML(node) {
-    let html = '';
+  separateMarks(input) {
+    const matches = [];
+    let currentIndex = 0;
 
-    // Handle different node types
-    switch (node.type) {
-      case 'doc':
-        // Process the document's content
-        node.content.forEach((childNode) => {
-          html += renderHTML(childNode);
-        });
-        break;
+    input.replace(matchMarkRegex, (match, delimiter, content, index) => {
+      const textBeforeMatch = input.substring(currentIndex, index);
+      if (textBeforeMatch) {
+        matches.push(textBeforeMatch);
+      }
+      if (match.match(/\[\[@\w+\|\w+\]\]/g)) {
+        matches.push(match);
+      } else if (match.startsWith('@') || match.match(/\[.*?\]\(.*?\)/g)) {
+        matches.push(match);
+      } else {
+        matches.push(`${delimiter}${content}${delimiter}`);
+      }
+      currentIndex = index + match.length;
+    });
 
-      case 'paragraph':
-        // Handle paragraph element
-        html += '<p>';
-        if (node.content) {
-          node.content.forEach((childNode) => {
-            html += renderHTML(childNode);
-          });
-        }
-        html += '</p>';
-        break;
-
-      case 'heading':
-        // Handle heading element with levels
-        const level = node.attrs?.level || 1;
-        html += `<h${level}>`;
-        if (node.content) {
-          node.content.forEach((childNode) => {
-            html += renderHTML(childNode);
-          });
-        }
-        html += `</h${level}>`;
-        break;
-
-      case 'bulletList':
-        // Handle unordered list
-        html += '<ul>';
-        node.content.forEach((childNode) => {
-          html += renderHTML(childNode);
-        });
-        html += '</ul>';
-        break;
-
-      case 'listItem':
-        // Handle list item
-        html += '<li>';
-        if (node.content) {
-          node.content.forEach((childNode) => {
-            html += renderHTML(childNode);
-          });
-        }
-        html += '</li>';
-        break;
-
-      case 'text':
-        // Handle text node
-        html += node.text || '';
-        break;
-
-      default:
-        break;
+    const remainingText = input.substring(currentIndex);
+    if (remainingText) {
+      matches.push(remainingText);
     }
+    return matches;
+  }
 
-    return html;
+  convertToTiptapJSON(text) {
+    const sections = text?.split('\n'); // Split the text into sections based on double line breaks
+    const tiptapJSON = {
+      type: 'doc',
+      content: [],
+    };
+
+    const applyMarks = (text) => {
+      let currentLine = text.trim();
+      const paragraphText = [];
+      const match = currentLine.match(matchMarkRegex);
+
+      if (match) {
+        const marks = separateMarks(currentLine);
+        for (const mark of marks) {
+          if (mark.match(/\[\[@\w+\|\w+\]\]/)) {
+            const id = mark
+              .match(/\[\[@\w+\|\w+\]\]/)[0]
+              ?.replace('[[@', '')
+              ?.replace(']]', '')
+              ?.split('|')[0];
+            const user = mentionables.value[id];
+            paragraphText.push({
+              type: 'mention',
+              attrs: { id: user },
+              label: null,
+            });
+          } else {
+            paragraphText.push({ type: 'text', text: mark });
+          }
+        }
+      } else {
+        paragraphText.push({ type: 'text', text: currentLine });
+      }
+
+      return {
+        type: 'paragraph',
+        content: paragraphText,
+      };
+    };
+    if (!sections) {
+      return tiptapJSON;
+    }
+    for (const section of sections) {
+      if (section.startsWith('- ')) {
+        // If the section starts with '-', it represents an ordered list
+        const listItems = section
+          .split('- ')
+          .map((item) => item.trim())
+          .filter((item) => item.length > 0);
+        tiptapJSON.content.push({
+          type: 'bulletList',
+          content: listItems.map((item) => ({
+            type: 'listItem',
+            content: [applyMarks(item)],
+          })),
+        });
+      } else if (!section) {
+        // If the section is empty, it represents a line break
+        tiptapJSON.content.push({
+          type: 'paragraph',
+        });
+      } else {
+        // Otherwise, it represents a paragraph
+        tiptapJSON.content.push(applyMarks(section));
+      }
+    }
+    return tiptapJSON;
+  }
+
+  // use convertFromTiptapJSON(json.content)
+  convertFromTiptapJSON(content) {
+    let text = '';
+
+    const processContent = (content) => {
+      for (const contentItem of content) {
+        if (contentItem.type === 'bulletList') {
+          for (const listItem of contentItem.content) {
+            text += `- ${processParagraph(listItem.content[0])}\n`;
+          }
+        } else if (contentItem.type === 'paragraph') {
+          text += processParagraph(contentItem) + '\n';
+        }
+      }
+    };
+
+    const processParagraph = (paragraph) => {
+      if (!paragraph.content) {
+        return '';
+      }
+
+      return paragraph.content
+        .filter((textNode) => textNode.type === 'text')
+        .map((textNode) => textNode.text)
+        .join(' ')
+        .trim();
+    };
+
+    processContent(content);
+
+    return text.trim();
   }
 }
 
